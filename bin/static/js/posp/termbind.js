4ac526180ea2d153bbd3b31c241658e679a46989
邓成 <dengcheng@qfpay.com>
2017-11-26 22:30:04 +0800

    add

--- bin/static/js/posp/termbind.js
+++ bin/static/js/posp/termbind.js
@@ -455,4 +455,221 @@ $(document).ready(function(){
         });
     });
 
+    $('#termbindViewSubmit').click(function(){
+
+        var termbind_edit_vt = $('#termbindViewForm').validate({
+            rules: {
+                active_date_view: {
+                    required: true,
+                    maxlength: 20
+                },
+                userid_view: {
+                    required: true,
+                    maxlength: 20
+                },
+                udid_view: {
+                    required: true,
+                    maxlength: 60
+                },
+                terminal_id_view: {
+                    required: true,
+                    maxlength: 20
+                },
+                psamid_view: {
+                    required: true,
+                    maxlength: 8
+                },
+                psamtp_view: {
+                    required: true,
+                    maxlength: 8
+                },
+                tckkey_view: {
+                    required: true,
+                    maxlength: 32
+                },
+                pinkey1_view: {
+                    required: true,
+                    maxlength: 32
+                },
+                pinkey2_view: {
+                    required: true,
+                    maxlength: 32
+                },
+                mackey_view: {
+                    required: true,
+                    maxlength: 32
+                },
+                diskey_view: {
+                    required: true,
+                    maxlength: 32
+                },
+                fackey_view: {
+                    required: true,
+                    maxlength: 32
+                },
+                enc_pin_key_view: {
+                    required: true,
+                    maxlength: 32
+                },
+                tmk_view: {
+                    required: true,
+                    maxlength: 32
+                },
+                os_ver_view: {
+                    required: true,
+                    maxlength: 8,
+                    digits: true
+                },
+                key_version_view: {
+                    required: true,
+                    maxlength: 8
+                },
+                qpos_pubkey_view: {
+                    required: true,
+                    maxlength: 256
+                },
+                dig_env_view: {
+                    required: true,
+                    maxlength: 256
+                }
+
+            },
+            messages: {
+                active_date_view: {
+                    required: '请选择激活日期'
+                },
+                userid_view: {
+                    required: '请输入商户ID',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                terminal_id_view: {
+                    required: '请输入终端ID',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                psamid_view: {
+                    required: '请输入psamid',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                psamtp_view: {
+                    required: '请输入psamtp',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                tckkey_view: {
+                    required: '请输入tckkey',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                pinkey1_view: {
+                    required: '请输入pinkey1',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                pinkey2_view: {
+                    required: '请输入pinkey2',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                mackey_view: {
+                    required: '请输入mackey',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                diskey_view: {
+                    required: '请输入diskey',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                fackey_view: {
+                    required: '请输入fackey',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                enc_pin_key_view: {
+                    required: '请输入enc_pin_key',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                tmk_view: {
+                    required: '请输入tmk',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                os_ver_view: {
+                    required: '请输入系统版本',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串"),
+                    digits: '请输入整数'
+                },
+                key_version_view: {
+                    required: '请输入秘钥版本',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                dig_env_view: {
+                    required: '请输入dig_env',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                qpos_pubkey_view: {
+                    required: '请输入公钥',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                }
+
+            },
+            errorPlacement: function(error, element){
+                if(element.is(':checkbox')){
+                    error.appendTo(element.parent().parent().parent());
+                } else {
+                    error.insertAfter(element);
+                }
+            }
+        });
+        var ok = termbind_edit_vt.form();
+        if(!ok){
+            return false;
+        }
+
+        var post_data = {};
+        var termbind_id = $('#view_termbind_id').text();
+        var se_userid = window.localStorage.getItem('myid');
+
+        post_data.se_userid = se_userid;
+        post_data.termbind_id = termbind_id;
+
+        post_data.active_date = $('#active_date_view').val();
+        post_data.userid = $('#userid_view').val();
+        post_data.udid = $('#udid_view').val();
+        post_data.terminalid = $('#terminal_id_view').val();
+        post_data.psamid = $('#psamid_view').val();
+        post_data.psamtp = $('#psamtp_view').val();
+        post_data.tckkey = $('#tckkey_view').val();
+        post_data.pinkey1 = $('#pinkey1_view').val();
+        post_data.pinkey2 = $('#pinkey2_view').val();
+        post_data.mackey = $('#mackey_view').val();
+        post_data.diskey = $('#diskey_view').val();
+        post_data.fackey = $('#fackey_view').val();
+        post_data.enc_pin_key = $('#enc_pin_key_view').val();
+        post_data.tmk = $('#tmk_view').val();
+        post_data.os = $('#os_view').val();
+        post_data.os_ver = $('#os_ver_view').val();
+        post_data.key_version = $('#key_version_view').val();
+        post_data.dig_env = $('#dig_env_view').val();
+        post_data.qpos_pubkey = $('#qpos_pubkey_view').val();
+        post_data.state = $('#state_view').val();
+
+        $.ajax({
+            url: '/posp/v1/api/terminal/bind/view',
+            type: 'POST',
+            dataType: 'json',
+            data: post_data,
+            success: function(data) {
+                var respcd = data.respcd;
+                if(respcd != '0000'){
+                    var resperr = data.resperr;
+                    var respmsg = data.respmsg;
+                    var msg = resperr ? resperr : respmsg;
+                    toastr.warning(msg);
+                    return false;
+                }
+                else {
+                    toastr.success('保存修改成功');
+                    $("#termbindlViewForm").resetForm();
+                    $("#termbindViewModal").modal('hide');
+                    $('#termbindList').DataTable().draw();
+                }
+            },
+            error: function(data) {
+                toastr.warning('请求异常');
+            }
+        });
+    });
 });
