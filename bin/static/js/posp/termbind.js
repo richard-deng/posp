0d4f9ba1927321b6d6d0fd3f0e1c46124f1a92e7
邓成 <dengcheng@qfpay.com>
2017-11-26 21:05:43 +0800

    add

--- bin/static/js/posp/termbind.js
+++ bin/static/js/posp/termbind.js
@@ -182,4 +182,212 @@ $(document).ready(function(){
         $("#termbindCreateModal").modal();
     });
 
+    $("#termbindCreateSubmit").click(function(){
+
+        var termbind_create_vt = $('#termbindCreateForm').validate({
+            rules: {
+                active_date_add: {
+                    required: true,
+                    maxlength: 20
+                },
+                userid_add: {
+                    required: true,
+                    maxlength: 20
+                },
+                terminal_id_add: {
+                    required: true,
+                    maxlength: 20
+                },
+                psamid_add: {
+                    required: true,
+                    maxlength: 8
+                },
+                psamtp_add: {
+                    required: true,
+                    maxlength: 8
+                },
+                tckkey_add: {
+                    required: true,
+                    maxlength: 32
+                },
+                pinkey1_add: {
+                    required: true,
+                    maxlength: 32
+                },
+                pinkey2_add: {
+                    required: true,
+                    maxlength: 32
+                },
+                mackey_add: {
+                    required: true,
+                    maxlength: 32
+                },
+                diskey_add: {
+                    required: true,
+                    maxlength: 32
+                },
+                fackey_add: {
+                    required: true,
+                    maxlength: 32
+                },
+                enc_pin_key_add: {
+                    required: true,
+                    maxlength: 32
+                },
+                tmk_add: {
+                    required: true,
+                    maxlength: 32
+                },
+                os_ver_add: {
+                    required: true,
+                    maxlength: 8
+                },
+                key_version_add: {
+                    required: true,
+                    maxlength: 8
+                },
+                qpos_pubkey_add: {
+                    required: true,
+                    maxlength: 256
+                },
+                dig_env_add: {
+                    required: true,
+                    maxlength: 256
+                }
+            },
+            messages: {
+                active_date_add: {
+                    required: '请选择激活日期'
+                },
+                userid_add: {
+                    required: '请输入商户ID',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                terminal_id_add: {
+                    required: '请输入终端ID',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                psamid_add: {
+                    required: '请输入psamid',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                psamtp_add: {
+                    required: '请输入psamtp',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                tckkey_add: {
+                    required: '请输入tckkey',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                pinkey1_add: {
+                    required: '请输入pinkey1',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                pinkey2_add: {
+                    required: '请输入pinkey2',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                mackey_add: {
+                    required: '请输入mackey',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                diskey_add: {
+                    required: '请输入diskey',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                fackey_add: {
+                    required: '请输入fackey',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                enc_pin_key_add: {
+                    required: '请输入enc_pin_key',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                tmk_add: {
+                    required: '请输入tmk',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                os_ver_add: {
+                    required: '请输入系统版本',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                key_version_add: {
+                    required: '请输入秘钥版本',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                dig_env_add: {
+                    required: '请输入dig_env',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                },
+                qpos_pubkey_add: {
+                    required: '请输入公钥',
+                    maxlength: $.validator.format("请输入一个 长度最多是 {0} 的字符串")
+                }
+
+            },
+            errorPlacement: function(error, element){
+                if(element.is(':checkbox')){
+                    error.appendTo(element.parent().parent().parent());
+                } else {
+                    error.insertAfter(element);
+                }
+            }
+        });
+
+        var ok = termbind_create_vt.form();
+        if(!ok){
+            return false;
+        }
+
+        var post_data = {};
+        var se_userid = window.localStorage.getItem('myid');
+
+        post_data.se_userid = se_userid;
+        post_data.active_date = $('#active_date_add').val();
+        post_data.userid = $('#userid_add').val();
+        post_data.terminalid = $('#terminal_id_add').val();
+        post_data.psamid = $('#psamid_add').val();
+        post_data.psamtp = $('#psamtp_add').val();
+        post_data.tckkey = $('#tckkey_add').val();
+        post_data.pinkey1 = $('#pinkey1_add').val();
+        post_data.pinkey2 = $('#pinkey2_add').val();
+        post_data.mackey = $('#mackey_add').val();
+        post_data.diskey = $('#diskey_add').val();
+        post_data.fackey = $('#fackey_add').val();
+        post_data.enc_pin_key = $('#enc_pin_key_add').val();
+        post_data.tmk = $('#tmk_add').val();
+        post_data.os = $('#os_add').val();
+        post_data.os_ver = $('#os_ver_add').val();
+        post_data.key_version = $('#key_version_add').val();
+        post_data.dig_env = $('#dig_env_add').val();
+        post_data.qpos_pubkey = $('#qpos_pubkey_add').val();
+        post_data.state = $('#state_add').val();
+
+        $.ajax({
+            url: '/posp/v1/api/terminal/bind/create',
+            type: 'POST',
+            dataType: 'json',
+            data: post_data,
+            success: function(data) {
+                var respcd = data.respcd;
+                if(respcd != '0000'){
+                    var resperr = data.resperr;
+                    var respmsg = data.respmsg;
+                    var msg = resperr ? resperr : respmsg;
+                    toastr.warning(msg);
+                    return false;
+                }
+                else {
+                    toastr.success('添加终端绑定成功');
+                    $("#termbindCreateModal").modal('hide');
+                    location.reload();
+                    $('#termbindList').DataTable().draw();
+                }
+            },
+            error: function(data) {
+                toastr.warning('请求异常');
+            }
+        });
+    });
+
 });
